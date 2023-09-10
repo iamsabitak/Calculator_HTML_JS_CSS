b6f11e53d00faceef4ba042d27e567eeca5f5e33
sabita <sabitakhadka091@gmail.com>
2023-09-10 18:40:46 +0545

    new file added

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let number1;
       let result = 0;
       let num1arr = [];
       let num2arr = [];
-      x = 0;
+      let x = 0;
       // let a = document.getElementById("result")
       function display(n) {
         console.log(n);
@@ -20,22 +20,25 @@ let number1;
           document.getElementById("result").value = number2;
         }
       }
-      function onClear() {
-        document.getElementById("result").value = 0;
-        number1 = null;
-        number2 = null;
-        operation = null;
-        num1arr = [];
-        num2arr = [];
-        result = 0;
-        x = 0;
-      }
       function operator(op) {
         // console.log(op)
         x = 2;
         operation = op;
       }
+      function onClear() {
+        // console.log("Clear console here");
+        document.getElementById("result").value = 0;
+        // number1 = null;
+        // number2 = null;
+        // operation = null;
+        num1arr = [];
+        num2arr = [];
+        result = 0;
+        x = 0;
+      }
+      
       function calculate() {
+        // console.log("Calculate here");
         if (operation === "+") {
           result = number1 + number2;
           document.getElementById("result").value = result;
